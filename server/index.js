ad78b8fe76991d3ad4ba2227efcb8f7e42e96dcd
Dmitry Dorofeev <d.dorofeev@corp.mail.ru>
2020-09-07 21:04:44 +0300

    add debug

--- server/index.js
+++ server/index.js
@@ -1,31 +1,32 @@
 const fs = require('fs');
 const http = require('http');
+const debug = require('debug')('http')
 
 const server = http.createServer((req, res) => {
-  console.log('requested', req.url);
+  debug('requested', req.url);
 
   const path = `./public${req.url === '/' ? '/index.html' : req.url}`;
 
   const ip = res.socket.remoteAddress;
   const port = res.socket.remotePort;
-  console.log(`Your IP address is ${ip} and your source port is ${port}.`);
+  debug(`Your IP address is ${ip} and your source port is ${port}.`);
 
   fs.readFile(path, (err, file) => {
     if (err) {
-      console.log('file read error', path, err);
+      debug('file read error', path, err);
       res.write('error');
       res.end();
 
       return;
     }
 
-    console.log('file read', path);
+    debug('file read', path);
 
     res.write(file);
     res.end();
   });
 
-  console.log('after read file')
+  debug('after read file');
 });
 
 server.listen(3000);
